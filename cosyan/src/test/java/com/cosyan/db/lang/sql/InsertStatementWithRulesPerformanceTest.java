7ec0dd8ed841ff635b0fce3fa1d0f33b29e75e0a
gsvigruha <gergely.svigruha@lynxanalytics.com>
2018-03-14 22:23:15 -0400

    Add insert test rev rule.

--- cosyan/src/test/java/com/cosyan/db/lang/sql/InsertStatementWithRulesPerformanceTest.java
+++ cosyan/src/test/java/com/cosyan/db/lang/sql/InsertStatementWithRulesPerformanceTest.java
@@ -26,4 +26,24 @@ public class InsertStatementWithRulesPerformanceTest extends UnitTestBase {
     t = System.currentTimeMillis() - t;
     System.out.println("Records with rules inserted in " + t);
   }
+
+  @Test
+  public void testInsertWithReverseRefRule() {
+    execute("create table t3 (a varchar, constraint pk_a primary key (a));");
+    execute("create table t4 (a varchar, b integer, "
+        + "constraint fk_a foreign key (a) references t3(a));");
+    execute("alter table t3 add ref s (select sum(b) as sb from rev_fk_a);");
+    execute("alter table t3 add constraint c_1 check (s.sb <= 10);");
+
+    for (int i = 0; i < N1; i++) {
+      execute("insert into t3 values ('abc" + i + "');");
+    }
+    long t = System.currentTimeMillis();
+    for (int i = 0; i < N2; i++) {
+      int j = i % N1;
+      execute("insert into t4 values ('abc" + j + "', 1);");
+    }
+    t = System.currentTimeMillis() - t;
+    System.out.println("Records with rules inserted in " + t);
+  }
 }
