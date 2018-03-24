98fc3ba029c233b93faa8483513dcc003f221db9
gsvigruha <gergely.svigruha@lynxanalytics.com>
2018-03-23 21:34:37 -0400

    Add IDIndex.

---
+++ cosyan/src/main/java/com/cosyan/db/index/IDIndex.java
@@ -0,0 +1,129 @@
+package com.cosyan.db.index;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+import java.util.Arrays;
+import java.util.TreeMap;
+
+import com.cosyan.db.index.ByteTrie.IndexException;
+
+public class IDIndex {
+
+  private static final int SIZE = 65536;
+
+  private final TreeMap<Long, long[]> cachedIndices = new TreeMap<>();
+
+  private final String fileName;
+
+  private RandomAccessFile raf;
+  private long filePointer;
+
+  public IDIndex(String fileName) throws IOException {
+    this.fileName = fileName;
+    this.raf = new RandomAccessFile(fileName, "rw");
+    filePointer = raf.length();
+  }
+
+  public void close() throws IOException {
+    cleanUp();
+    raf.close();
+  }
+
+  public void cleanUp() {
+    cachedIndices.clear();
+  }
+
+  public void drop() throws IOException {
+    close();
+    new File(fileName).delete();
+  }
+
+  public void reOpen() throws FileNotFoundException {
+    this.raf = new RandomAccessFile(fileName, "rw");
+  }
+
+  public Long get(long key) throws IOException {
+    long segment = key / SIZE;
+    long[] cachedValues = cachedIndices.get(segment);
+    if (cachedValues == null) {
+      if (key >= filePointer) {
+        return null;
+      }
+      cachedValues = read(segment);
+    }
+    long cachedValue = cachedValues[(int) (key % SIZE)];
+    return cachedValue == -1 ? null : cachedValue;
+  }
+
+  private long[] read(long segment) throws IOException {
+    ByteBuffer bb = ByteBuffer.allocate(SIZE * 8);
+    raf.seek(segment * SIZE * 8);
+    raf.read(bb.array());
+    LongBuffer lb = bb.asLongBuffer();
+    long[] cachedValues = new long[lb.capacity()];
+    lb.get(cachedValues);
+    return cachedValues;
+  }
+
+  public void put(long key, long value) throws IOException, IndexException {
+    long segment = key / SIZE;
+    long[] cachedValues = cachedIndices.get(segment);
+    if (cachedValues == null) {
+      if (key >= filePointer) {
+        cachedValues = new long[SIZE];
+        Arrays.fill(cachedValues, -1);
+      } else {
+        cachedValues = read(segment);
+      }
+      cachedIndices.put(segment, cachedValues);
+    }
+    if (cachedValues[(int) (key % SIZE)] != -1) {
+      throw new IndexException("Key '" + key + "' already present in index.");
+    }
+    cachedValues[(int) (key % SIZE)] = value;
+  }
+
+  public boolean delete(long key) throws IOException {
+    long segment = key / SIZE;
+    long[] cachedValues = cachedIndices.get(segment);
+    long blockStart = segment * SIZE;
+    if (cachedValues == null) {
+      if (key >= filePointer) {
+        return false;
+      }
+      cachedValues = read(segment);
+      cachedIndices.put(segment, cachedValues);
+    }
+    int idx = (int) (key - blockStart);
+    long cachedValue = cachedValues[idx];
+    if (cachedValue == -1) {
+      return false;
+    } else {
+      cachedValues[idx] = -1;
+      return true;
+    }
+  }
+
+  public void commit() throws IOException {
+    for (long i = 0; i <= cachedIndices.lastKey(); i++) {
+      long[] values = cachedIndices.get(i);
+      if (values == null) {
+        values = new long[SIZE];
+        Arrays.fill(values, -1);
+      }
+      raf.seek(i * SIZE * 8);
+      ByteBuffer lb = ByteBuffer.allocate(SIZE * 8);
+      lb.asLongBuffer().put(values);
+      raf.write(lb.array());
+    }
+    filePointer = raf.length();
+  }
+
+  public void rollback() {
+    cachedIndices.clear();
+  }
+}
---
+++ cosyan/src/test/java/com/cosyan/db/index/IDIndexTest.java
@@ -0,0 +1,130 @@
+package com.cosyan.db.index;
+
+import static org.junit.Assert.fail;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import com.cosyan.db.index.ByteTrie.IndexException;
+
+public class IDIndexTest {
+
+  private void assertEquals(Long expected, Long actual) {
+    org.junit.Assert.assertEquals(expected, actual);
+  }
+
+  private void assertEquals(boolean expected, boolean actual) {
+    org.junit.Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testIDIndex() throws Exception {
+    Files.deleteIfExists(Paths.get("/tmp/idindex"));
+    IDIndex index = new IDIndex("/tmp/idindex");
+    assertEquals(null, index.get(1L));
+    index.put(1L, 10L);
+    index.commit();
+    assertEquals(10L, index.get(1L));
+    index.put(2L, 20L);
+    index.commit();
+    assertEquals(20L, index.get(2L));
+    index.put(3L, 30L);
+    index.commit();
+    assertEquals(30L, index.get(3L));
+    index.put(9999999L, 40L);
+    index.commit();
+    assertEquals(40L, index.get(9999999L));
+    assertEquals(20L, index.get(2L));
+    assertEquals(30L, index.get(3L));
+    assertEquals(10L, index.get(1L));
+    assertEquals(null, index.get(666L));
+
+    index.put(100000L, 50L);
+    assertEquals(50L, index.get(100000L));
+
+    assertEquals(true, index.delete(3L));
+    index.commit();
+    assertEquals(null, index.get(3L));
+    assertEquals(false, index.delete(5L));
+    index.commit();
+    assertEquals(true, index.delete(2L));
+    index.commit();
+    assertEquals(null, index.get(2L));
+
+    assertEquals(10L, index.get(1L));
+    assertEquals(40L, index.get(9999999L));
+    assertEquals(50L, index.get(100000L));
+  }
+
+  @Test
+  public void testIDIndexCommitAndRollback() throws Exception {
+    Files.deleteIfExists(Paths.get("/tmp/idindex"));
+    IDIndex index = new IDIndex("/tmp/idindex");
+    assertEquals(null, index.get(1L));
+    index.put(10L, 10L);
+    index.put(30L, 30L);
+    index.put(20L, 20L);
+    assertEquals(10L, index.get(10L));
+    assertEquals(20L, index.get(20L));
+    assertEquals(30L, index.get(30L));
+    index.commit();
+    assertEquals(10L, index.get(10L));
+    assertEquals(20L, index.get(20L));
+    assertEquals(30L, index.get(30L));
+
+    index.put(50L, 50L);
+    index.put(40L, 40L);
+    assertEquals(10L, index.get(10L));
+    assertEquals(20L, index.get(20L));
+    assertEquals(30L, index.get(30L));
+    assertEquals(40L, index.get(40L));
+    assertEquals(50L, index.get(50L));
+    index.rollback();
+
+    assertEquals(10L, index.get(10L));
+    assertEquals(20L, index.get(20L));
+    assertEquals(30L, index.get(30L));
+    assertEquals(null, index.get(40L));
+    assertEquals(null, index.get(50L));
+
+    index.delete(20L);
+    assertEquals(10L, index.get(10L));
+    assertEquals(null, index.get(20L));
+    assertEquals(30L, index.get(30L));
+    index.commit();
+    assertEquals(10L, index.get(10L));
+    assertEquals(null, index.get(20L));
+    assertEquals(30L, index.get(30L));
+
+    index.delete(10L);
+    assertEquals(null, index.get(10L));
+    assertEquals(30L, index.get(30L));
+
+    index.rollback();
+    assertEquals(10L, index.get(10L));
+    assertEquals(30L, index.get(30L));
+
+    index.put(60L, 60L);
+    assertEquals(60L, index.get(60L));
+    index.delete(60L);
+    assertEquals(null, index.get(60L));
+    index.commit();
+    assertEquals(null, index.get(60L));
+  }
+
+  @Test
+  public void testIDIndexDuplicateKeys() throws Exception {
+    Files.deleteIfExists(Paths.get("/tmp/idindex"));
+    IDIndex index = new IDIndex("/tmp/idindex");
+    assertEquals(null, index.get(1L));
+    index.put(1L, 10L);
+    assertEquals(10L, index.get(1L));
+    try {
+      index.put(1L, 10L);
+      fail();
+    } catch (IndexException e) {
+    }
+  }
+}
