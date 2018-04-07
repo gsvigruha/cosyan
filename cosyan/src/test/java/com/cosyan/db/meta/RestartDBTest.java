0d23419ed7d91c268e998d0c3ec5d9875b605a52
gsvigruha <gergely.svigruha@lynxanalytics.com>
2018-04-07 00:29:28 -0400

    Add tests.

--- cosyan/src/test/java/com/cosyan/db/meta/RestartDBTest.java
+++ cosyan/src/test/java/com/cosyan/db/meta/RestartDBTest.java
@@ -1,6 +1,7 @@
 package com.cosyan.db.meta;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,13 +13,17 @@ import org.junit.Test;
 
 import com.cosyan.db.DBApi;
 import com.cosyan.db.conf.Config;
+import com.cosyan.db.lang.transaction.Result.ErrorResult;
+import com.cosyan.db.lang.transaction.Result.QueryResult;
+import com.cosyan.db.lang.transaction.Result.TransactionResult;
 import com.cosyan.db.meta.MetaRepo.ModelException;
 import com.cosyan.db.model.Ident;
 import com.cosyan.db.model.MaterializedTableMeta;
 import com.cosyan.db.session.IParser.ParserException;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 
-public class MetaRepoTest {
+public class RestartDBTest {
 
   private static Config config;
 
@@ -41,6 +46,7 @@ public class MetaRepoTest {
     assertEquals(1, tableMeta.columns().size());
     assertEquals(1, tableMeta.rules().size());
     assertEquals(true, tableMeta.primaryKey().isPresent());
+    assertTrue(dbApi.getMetaRepo().uniqueIndexNames().contains("t1.a"));
     dbApi = new DBApi(config);
     MaterializedTableMeta newTableMeta = dbApi.getMetaRepo().table(new Ident("t1"));
     assertEquals(tableMeta.columns(), newTableMeta.columns());
@@ -48,6 +54,7 @@ public class MetaRepoTest {
     assertEquals(tableMeta.primaryKey(), newTableMeta.primaryKey());
     assertEquals(tableMeta.foreignKeys(), newTableMeta.foreignKeys());
     assertEquals(tableMeta.reverseForeignKeys(), newTableMeta.reverseForeignKeys());
+    assertTrue(dbApi.getMetaRepo().uniqueIndexNames().contains("t1.a"));
   }
 
   @Test
@@ -127,5 +134,46 @@ public class MetaRepoTest {
     assertEquals(1, newT7.refs().size());
     assertEquals("rev_fk_a1", newT7.refs().get("s").getTableMeta().getReverseForeignKey().getName());
     assertEquals(1, newT7.rules().size());
+  }
+
+  @Test
+  public void testDoubleRestart() throws IOException, ModelException, ParserException {
+    DBApi dbApi = new DBApi(config);
+    dbApi.getSession().execute("create table t9(a integer, b varchar);");
+
+    MaterializedTableMeta t9 = dbApi.getMetaRepo().table(new Ident("t9"));
+    assertEquals(2, t9.columns().size());
+
+    dbApi = new DBApi(config);
+    MaterializedTableMeta t9_2 = dbApi.getMetaRepo().table(new Ident("t9"));
+    assertEquals(2, t9_2.columns().size());
+
+    dbApi = new DBApi(config);
+    MaterializedTableMeta t9_3 = dbApi.getMetaRepo().table(new Ident("t9"));
+    assertEquals(2, t9_3.columns().size());
+  }
+
+  @Test
+  public void testTableDataAfterRestart() throws IOException, ModelException, ParserException {
+    DBApi dbApi = new DBApi(config);
+    dbApi.getSession().execute("create table t10(a integer, b varchar);");
+    dbApi.getSession().execute("insert into t10 values(1, 'x');");
+
+    dbApi = new DBApi(config);
+    QueryResult result = (QueryResult) ((TransactionResult) dbApi.getSession().execute("select * from t10;"))
+        .getResults().get(0);
+    assertEquals(ImmutableList.of("a", "b"), result.getHeader());
+    assertEquals(ImmutableList.of(ImmutableList.of(1L, "x")), result.getValues());
+  }
+
+  @Test
+  public void testIndexesAfterRestart() throws IOException, ModelException, ParserException {
+    DBApi dbApi = new DBApi(config);
+    dbApi.getSession().execute("create table t11(a integer unique);");
+    dbApi.getSession().execute("insert into t11 values(1);");
+
+    dbApi = new DBApi(config);
+    ErrorResult result = (ErrorResult) dbApi.getSession().execute("insert into t11 values(1);");
+    assertEquals("Key '1' already present in index.", result.getError().getMessage());
   }
 }
