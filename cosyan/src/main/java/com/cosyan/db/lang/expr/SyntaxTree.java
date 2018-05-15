dcfa60be27ea6514c4f244f19a10b904e2b86e72
gsvigruha <gergely.svigruha@lynxanalytics.com>
2018-05-15 19:39:00 -0400

    ST.

--- cosyan/src/main/java/com/cosyan/db/lang/expr/SyntaxTree.java
+++ cosyan/src/main/java/com/cosyan/db/lang/expr/SyntaxTree.java
@@ -14,15 +14,13 @@ import com.cosyan.db.model.DataTypes.DataType;
 import com.cosyan.db.transaction.MetaResources;
 import com.cosyan.db.transaction.Resources;
 
-import lombok.Data;
-
 public class SyntaxTree {
 
   public static enum AggregationExpression {
     YES, NO, EITHER
   }
 
-  @Data
+  @lombok.Data
   public static abstract class Node {
 
   }
