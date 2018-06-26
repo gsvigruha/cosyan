44d88a3b79b8b0dd001763efee2b175db0b5947c
gsvigruha <gergely.svigruha@lynxanalytics.com>
2018-06-25 23:16:06 -0400

    Remove cols from UI.

--- cosyan/web/app/admin/admin.js
+++ cosyan/web/app/admin/admin.js
@@ -11,11 +11,39 @@ angular.module('cosyan')
   $scope.tableLoc = {};
   
   $scope.loadTables = function() {
-    $http.get("/cosyan/admin", {
-        params: { user: sessionStorage.getItem('user') }
+	$http.get("/cosyan/admin", {
+      params: { user: sessionStorage.getItem('user') }
     }).then(function(response) {
       $scope.data = response.data;
+      if ($scope.activeTable) {
+        $scope.pickTable($scope.activeTable.name);
+      }
     });
   };
+  
+  $scope.dropConstraint = function(name) {
+    var query = 'alter table ' + $scope.activeTable.name + ' drop constraint ' + name + ';';
+	$http.get("/cosyan/sql", {
+	  params: { sql: query, user: sessionStorage.getItem('user') }
+	}).then(function success(response) {
+      $scope.$error = undefined;
+      $scope.loadTables();
+    }, function error(response) {
+      $scope.$error = response.data.error;
+    });
+  };
+  
+  $scope.dropColumn = function(name) {
+    var query = 'alter table ' + $scope.activeTable.name + ' drop ' + name + ';';
+	$http.get("/cosyan/sql", {
+	  params: { sql: query, user: sessionStorage.getItem('user') }
+	}).then(function success(response) {
+	  $scope.$error = undefined;
+	  $scope.loadTables();
+    }, function error(response) {
+	  $scope.$error = response.data.error;
+    });
+  };
+  
   $scope.loadTables();
 });
