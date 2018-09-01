5db04378b6578b85008edbb7c34c8b060aefd18b
gsvigruha <sgergo88@gmail.com>
2018-09-01 19:45:56 -0400

    Fix deploy script.

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -xue
 
+cd $(git rev-parse --show-toplevel)
 DEPLOY_DIR=deploy/cosyandb-$VERSION
 
 rm -rf $DEPLOY_DIR
 mkdir -p $DEPLOY_DIR
+gradle unitTest
 gradle fatJar
 cp build/libs/cosyan-all.jar $DEPLOY_DIR/
 cp -r web $DEPLOY_DIR/
-cp start.sh $DEPLOY_DIR/
-cp stop.sh $DEPLOY_DIR/
-cp restart.sh $DEPLOY_DIR/
+cp tools/start.sh $DEPLOY_DIR/
+cp tools/stop.sh $DEPLOY_DIR/
+cp tools/restart.sh $DEPLOY_DIR/
 cp -r conf $DEPLOY_DIR/
 cd deploy/
 tar -czf ~/cosyandb-$VERSION.tar.gz cosyandb-$VERSION
