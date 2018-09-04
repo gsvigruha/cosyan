e0c688edc95d00bb84309108b94025303fdf9975
Jonathan Levin <mail@jonathanlevin.co.uk>
2018-09-04 08:31:32 +0100

    first commit

--- src/main/java/com/cosyan/db/tools/LogicalBackup.java
+++ src/main/java/com/cosyan/db/tools/LogicalBackup.java
@@ -1,4 +1,9 @@
 package com.cosyan.db.tools;
 
 public class LogicalBackup {
+// TODO: Get the metadata of the database tables
+// TODO: Convert the metadata to CREATE TABLE commands
+// TODO: Have command to disable foreign key checking (to enable loading of data without going through them)
+// TODO: Go through all tables and generate INSERT statements for their contents
+
 }
